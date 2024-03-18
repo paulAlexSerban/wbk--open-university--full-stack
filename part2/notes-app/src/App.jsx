97ebc0792069bc165e62913d24c98ef32328e6c4
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-18 15:59:19 +0200

    part2(notes-app): coursenotes

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,12 +1,13 @@
 import Note from './components/Note';
-import axios from 'axios';
+import Notification from './components/Notification';
 import { useState, useEffect } from 'react';
 import noteService from './services/notes';
-
+import Footer from './components/Footer';
 const App = (props) => {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState('a new note...');
     const [showAll, setShowAll] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const hook = () => {
         noteService.getAll().then((initialNotes) => {
@@ -45,7 +46,10 @@ const App = (props) => {
                 setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
             })
             .catch((error) => {
-                alert(`the note '${note.content}' was already deleted from server`);
+                setErrorMessage(`Note '${note.content}' was already removed from server`);
+                setTimeout(() => {
+                    setErrorMessage(null);
+                }, 5000);
                 setNotes(notes.filter((n) => n.id !== id));
             });
     };
@@ -54,6 +58,7 @@ const App = (props) => {
     return (
         <div>
             <h1>Notes</h1>
+            {errorMessage && <Notification message={errorMessage} />}
             <div>
                 <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
             </div>
@@ -67,6 +72,7 @@ const App = (props) => {
                 <input value={newNote} onChange={handleNoteChange} />
                 <button type="submit">save</button>
             </form>
+            <Footer />
         </div>
     );
 };
--- part2/notes-app/src/main.jsx
+++ part2/notes-app/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
-
+import './index.scss';
 const notes = [
   {
     id: 1,
