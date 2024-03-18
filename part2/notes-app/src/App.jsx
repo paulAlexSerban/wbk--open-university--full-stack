e1eb988092e8324c4562430a67266d67ec29eeda
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-18 12:53:50 +0200

    part2(notes-app): coursenotes

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,9 +1,22 @@
 import Note from './components/Note';
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 const App = (props) => {
-    const [notes, setNotes] = useState(props.notes);
+    const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState('a new note...');
     const [showAll, setShowAll] = useState(true);
+
+    const hook = () => {
+        console.log('effect');
+        axios.get('http://localhost:5000/notes').then((response) => {
+            console.log('promise fulfilled');
+            setNotes(response.data);
+        });
+    };
+
+    useEffect(hook, []);
+    console.log('render', notes.length, 'notes');
 
     const addNote = (event) => {
         event.preventDefault();
