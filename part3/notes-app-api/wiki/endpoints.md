36171773f04cd4d2dc99d77d916bbdafe7cd6988
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-21 13:00:46 +0200

    part3(phonebook):3.1 - structure and get all phonebook entries

---
+++ part3/notes-app-api/wiki/endpoints.md
@@ -0,0 +1,7 @@
+| URL	     | verb   | functionality
+| notes/10 | GET    | fetches a single resource
+| notes	   | GET    | fetches all resources in the collection
+| notes	   | POST   | creates a new resource based on the request data
+| notes/10 | DELETE |	removes the identified resource
+| notes/10 | PUT    | replaces the entire identified resource with the request data
+| notes/10 | PATCH  | replaces a part of the identified resource with the request data
---
+++ part3/phonebook/backend/apis/phonebook-api/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const HOST = process.env.HOST || 'localhost';
+
+const app = express();
+
+let persons = [
+    {
+        id: 1,
+        name: 'Arto Hellas',
+        number: '040-123456',
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: '39-44-5323523',
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: '12-43-234345',
+    },
+    {
+        id: 4,
+        name: 'Mary Poppendieck',
+        number: '39-23-6423122',
+    },
+];
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    response.json({ message: 'phonebook api is working' });
+});
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
+});
