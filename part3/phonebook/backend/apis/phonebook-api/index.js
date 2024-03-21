cab7ab7357967ad1ea000bda02c01d97d44cfe26
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-21 13:08:30 +0200

    part3(phonebook):3.3 - get persons by id

--- part3/phonebook/backend/apis/phonebook-api/index.js
+++ part3/phonebook/backend/apis/phonebook-api/index.js
@@ -47,6 +47,17 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server running at http://${HOST}:${PORT}`);
 });
